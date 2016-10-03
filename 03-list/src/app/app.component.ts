8fbab35309e96e61fcd3d84f781f131368b30737
Lei Hua <leihua2009@gmail.com>
2016-10-02 22:22:01 -0400

    added image placeholder to hero list

---
+++ 03-list/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import '../style/app.scss';
+import { Hero } from './shared/models/hero.class';
+import { HEROES } from './shared/models/hero.model';
+
+@Component({
+  selector: 'my-app', // <my-app></my-app>
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'Tour of Hero';
+  hero: Hero = {
+    id: 1,
+    name: 'windstorm',
+    image: 'img/200x200.png'
+  };
+
+  heroes = HEROES;
+}
---
+++ 03-list/src/app/shared/models/hero.model.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero.class';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice', image: 'img/200x200.png' },
+    { id: 12, name: 'Narco', image: 'img/200x200.png' },
+    { id: 13, name: 'Bombasto', image: 'img/200x200.png' },
+    { id: 14, name: 'Celeritas', image: 'img/200x200.png' },
+    { id: 15, name: 'Magneta', image: 'img/200x200.png' },
+    { id: 16, name: 'RubberMan', image: 'img/200x200.png' },
+    { id: 17, name: 'Dynama', image: 'img/200x200.png' },
+    { id: 18, name: 'Dr IQ', image: 'img/200x200.png' },
+    { id: 19, name: 'Magma', image: 'img/200x200.png' },
+    { id: 20, name: 'Tornado', image: 'img/200x200.png' }
+];
